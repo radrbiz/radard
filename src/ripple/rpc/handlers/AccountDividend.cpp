061a5a8c37c4b3c917db0450f08442fe5f4e9ecc
russell2x2 <russell2x2@gmail.com>
2015-01-16 11:29:19 +0800

    accout_dividend result format modify

--- src/ripple/rpc/handlers/AccountDividend.cpp
+++ src/ripple/rpc/handlers/AccountDividend.cpp
@@ -7,8 +7,9 @@ Json::Value doAccountDividend (RPC::Context& context)
     {
         return ripple::RPC::make_error(rpcTXN_NOT_FOUND, "accountDividendNotFound");
     }
-    
+    Json::Value result;
     auto account = context.params_["account"].asString();
+    result["Account"] = account;
     
     Ledger::pointer ledger = getApp().getOPs().getClosedLedger();
     SLE::pointer dividendSLE = ledger->getDividendObject();
@@ -19,6 +20,7 @@ Json::Value doAccountDividend (RPC::Context& context)
         boost::str (boost::format ("SELECT AccountTransactions.TransID FROM AccountTransactions JOIN Transactions "
                                    "ON AccountTransactions.TransID=Transactions.TransID "
                                    "WHERE Account='%s' AND AccountTransactions.LedgerSeq>%d "
+                                   "AND TransType='Dividend' "
                                    "ORDER BY AccountTransactions.LedgerSeq ASC LIMIT 1;")
                     % account.c_str()
                     % baseLedgerSeq);
@@ -38,11 +40,27 @@ Json::Value doAccountDividend (RPC::Context& context)
         }
         if (txn)
         {
-            return txn->getJson(0);
+            result["DividendCoins"] = to_string(txn->getSTransaction()->getFieldU64(sfDividendCoins));
+            result["DividendCoinsVBC"] = to_string(txn->getSTransaction()->getFieldU64(sfDividendCoinsVBC));
+            result["DividendCoinsVBCRank"] = to_string(txn->getSTransaction()->getFieldU64(sfDividendCoinsVBCRank));
+            result["DividendCoinsVBCSprd"] = to_string(txn->getSTransaction()->getFieldU64(sfDividendCoinsVBCSprd));
+            result["DividendTSprd"] = to_string(txn->getSTransaction()->getFieldU64(sfDividendTSprd));
+            result["DividendVRank"] = to_string(txn->getSTransaction()->getFieldU64(sfDividendVRank));
+            result["DividendVSprd"] = to_string(txn->getSTransaction()->getFieldU64(sfDividendVSprd));
+            result["DividendLedger"] = to_string(txn->getSTransaction()->getFieldU32(sfDividendLedger));
+
+            return result;
         }
     }
-    
-    return rpcError (rpcTXN_NOT_FOUND);
+    result["DividendCoins"] = "0";
+    result["DividendCoinsVBC"] = "0";
+    result["DividendCoinsVBCRank"] = "0";
+    result["DividendCoinsVBCSprd"] = "0";
+    result["DividendTSprd"] = "0";
+    result["DividendVRank"] = "0";
+    result["DividendVSprd"] = "0";
+    result["DividendLedger"] = "0";
+    return result;
 }
 
 } // ripple
