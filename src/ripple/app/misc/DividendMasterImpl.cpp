32e473c588a67178e68db70734d62a5d408806e1
russell2x2 <russell2x2@gmail.com>
2015-03-31 11:19:16 +0800

    references field storage shift at dividend, this should only run once

--- src/ripple/app/misc/DividendMasterImpl.cpp
+++ src/ripple/app/misc/DividendMasterImpl.cpp
@@ -201,6 +201,7 @@ public:
 
     void fillDivResult(SHAMap::pointer initialPosition) override
     {
+        std::unordered_set<Account> processedAccounts;
         for (const auto& it : m_divResult) {
             SerializedTransaction trans(ttDIVIDEND);
             trans.setFieldU8(sfDividendType, DividendMaster::DivType_Apply);
@@ -226,12 +227,52 @@ public:
                     m_journal.warning << "Ledger already had dividend for " << std::get<0>(it);
             }
             else {
+                processedAccounts.insert(std::get<0>(it));
                 if (m_journal.trace.active())
                     m_journal.trace << "dividend add TX " << txID << " for " << std::get<0>(it);
             }
         }
+        
         if (m_journal.info)
             m_journal.info << "dividend add " << m_divResult.size() << " TXs done. Mem" << memUsed();
+        
+        //Refer storage shift
+        Ledger::ref baseLedger = getApp().getOPs().getValidatedLedger();
+        baseLedger->visitStateItems([this, &initialPosition, processedAccounts](SLE::ref sle) {
+            if (sle->getType() == ltACCOUNT_ROOT) {
+                Account account = sle->getFieldAccount(sfAccount).getAccountID();
+                // account not added yet and it has sfReferences field
+                if (processedAccounts.find(account) == processedAccounts.end() && sle->isFieldPresent(sfReferences)) {
+                    SerializedTransaction trans(ttDIVIDEND);
+                    trans.setFieldU8(sfDividendType, DividendMaster::DivType_Apply);
+                    trans.setFieldAccount(sfAccount, Account());
+                    trans.setFieldAccount(sfDestination, account);
+                    trans.setFieldU32(sfDividendLedger, m_dividendLedgerSeq);
+                    trans.setFieldU64(sfDividendCoins, 0);
+                    trans.setFieldU64(sfDividendCoinsVBC, 0);
+                    trans.setFieldU64(sfDividendCoinsVBCRank, 0);
+                    trans.setFieldU64(sfDividendCoinsVBCSprd, 0);
+                    trans.setFieldU64(sfDividendVRank, 0);
+                    trans.setFieldU64(sfDividendVSprd, 0);
+                    trans.setFieldU64(sfDividendTSprd, 0);
+                    
+                    uint256 txID = trans.getTransactionID();
+                    Serializer s;
+                    trans.add(s, true);
+                    
+                    SHAMapItem::pointer tItem = std::make_shared<SHAMapItem>(txID, s.peekData());
+                    
+                    if (!initialPosition->addGiveItem(tItem, true, false)) {
+                        if (m_journal.warning.active())
+                            m_journal.warning << "Refer Shift: fail for account: " << account;
+                    }
+                    else {
+                        if (m_journal.trace.active())
+                            m_journal.trace << "Refer Shift: ok for tx: " << txID << ", account: " << account;
+                    }
+                }
+            }
+        });
     }
 
     void setLedgerSeq(uint32_t seq) override
@@ -356,11 +397,15 @@ bool DividendMaster::calcDividendFunc(Ledger::ref baseLedger, uint64_t dividendC
     std::multimap<std::tuple<Account, Account, uint32_t>, std::tuple<uint64_t, uint32_t, uint64_t, uint64_t>, AccountsByReference_Less> accountsByReference;
     
     // visit account stats to fill accountsByBalance
-    baseLedger->visitStateItems([&accountsByBalance, &accountsByReference](SLE::ref sle) {
+    baseLedger->visitStateItems([&accountsByBalance, &accountsByReference, baseLedger](SLE::ref sle) {
         if (sle->getType() == ltACCOUNT_ROOT) {
             uint64_t bal = sle->getFieldAmount(sfBalanceVBC).getNValue();
-            if (bal < SYSTEM_CURRENCY_PARTS_VBC && (!sle->isFieldPresent(sfReferences)))
-                return;
+            if (bal < SYSTEM_CURRENCY_PARTS_VBC) {
+                auto selRefer = baseLedger->getReferObject(sle->getFieldAccount(sfAccount).getAccountID());
+                if (!selRefer && !sle->isFieldPresent(sfReferences)) {
+                    return;
+                }
+            }
             uint32_t height = 0;
             Account addrParent;
             if (sle->isFieldPresent(sfReferee) && sle->isFieldPresent(sfReferenceHeight)) {
